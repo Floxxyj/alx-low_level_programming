bc717de2fa7b99d02aa847c451acde723928eed7
Floxxyj <mjedozie@gmail.com>
2022-09-08 14:28:56 -0700

    print f

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,31 +1,14 @@
 #include <stdio.h>
 
 /**
+ * main - print the string in the functiom
  *
- * * main-Entry point
- *
- * *
- *
- * * Return: 0 if successful
- *
- * */
-
+ * Description: using the main function
+ * this program prints "programming is like building a multilingual puzzle
+ * Return: 0
+ */
 int main(void)
-
 {
-
-	printf("Size of a char: %d byte(s)\n", sizeof(char));
-
-	printf("Size of an int: %d byte(s)\n", sizeof(int));
-
-	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-
-	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-
-	printf("Size of a float: %d byte(s)\n", sizeof(float));
-
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 	return (0);
-
 }
-
-
