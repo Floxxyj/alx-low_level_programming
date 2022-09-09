9b638e199fca2b9404de3cb626e93e5c4d2fa7b8
Floxxyj <mjedozie@gmail.com>
2022-09-09 04:14:37 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - print the string in the functiom
- *
- * Description: using the main function
- * this program prints "programming is like building a multilingual puzzle
- * Return: 0
+ * main - prints the phrase with proper grammar,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art,\n");
+
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
