d995c72225e1ae03bdaf1351718801817d51ca7d
Floxxyj <mjedozie@gmail.com>
2022-10-12 14:36:56 -0700

    100-main_opcodes.c

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -26,8 +26,8 @@ int main(int argc, char *argv[])
 	}
 	add = (char *)&main;
 	bytes = atoi(argv[1]);
-i = 0;
-while (i < bytes - 1)
+	i = 0;
+	while (i < bytes - 1)
 	{
 		printf("%02hhx ", add[i]);
 		i++;
@@ -35,4 +35,3 @@ while (i < bytes - 1)
 	printf("%02hhx\n", add[i]);
 	return (0);
 }
-
